3a15cee2cd8ef1a19b901fb609a34c8306470412
Jairo Rodriguez <jairorodriguez.97@outlook.com>
2024-05-26 10:35:13 -0500

    Project structure creation

---
+++ src/ActivityLog.js
@@ -0,0 +1,12 @@
+import "./activityLog.css";
+import Home from "./components/Home";
+
+function ActivityLog() {
+  return (
+    <div className="ActivityLog">
+      <Home />
+    </div>
+  );
+}
+
+export default ActivityLog;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ActivityLog from "./ActivityLog";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ActivityLog />
+  </React.StrictMode>
+);
